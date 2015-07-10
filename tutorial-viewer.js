f8fd491ee93d53a1fa606a79e64c24870adf19cf
Sashko Stubailo <sashko@stubailo.com>
2015-07-09 17:21:08 -0700

    Add tabs for each tutorial

--- tutorial-viewer.js
+++ tutorial-viewer.js
@@ -1,7 +1,22 @@
 if (Meteor.isClient) {
+  ReactiveTabs.createInterface({
+    template: 'basicTabs'
+  });
+
   Template.body.helpers({
-    reactTutorial: function () {
-      return REACT_TUT;
+    tutorials: function () {
+      return {
+        BLAZE_TUT: BLAZE_TUT,
+        ANGULAR_TUT: ANGULAR_TUT,
+        REACT_TUT: REACT_TUT
+      };
+    },
+    tabs: function () {
+      return [
+        { name: "Blaze", slug: "blaze" },
+        { name: "Angular", slug: "angular" },
+        { name: "React", slug: "react" }
+      ]
     }
   });
 }
